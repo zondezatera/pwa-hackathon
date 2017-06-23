71dd93b6b09dc3ba9ebbd773ff210293e90c21e1
Zonde Zatera <zatera1920@gmail.com>
2017-06-24 05:31:47 +0700

    update style

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ class App extends Component {
       <div>
         <nav className="nav has-shadow">
           <div className="nav-left">
+            <span className="nav-toggle">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
             <a className="nav-item">
               <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-zondez.appspot.com/o/icon-96x96.png?alt=media&token=3eaeeea0-b9c3-4002-9b45-f31acef86515" alt="test" />
               Drunk Limit
             </a>
           </div>
           <div className="nav-center">
+          </div>
+          <div className="nav-right nav-menu">
             <a className="nav-item">
               <span className="icon">
                 test1
@@ -23,9 +30,10 @@ class App extends Component {
               </span>
             </a>
           </div>
-          <div className="nav-right nav-menu">
-          </div>
         </nav>
+        <div>
+          test
+        </div>
       </div>
     )
   }
