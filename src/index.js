ad0975d20555d90db59b5a311a796332772dd35d
Zonde Zatera <zatera1920@gmail.com>
2017-06-23 23:44:24 +0700

    choose theme , fix eslint  and clean code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import TestComponent from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <MuiThemeProvider>
+    <TestComponent />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
