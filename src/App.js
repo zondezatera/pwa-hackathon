db166903c90a374aa62b474ae6762f43a6c13ee0
Zonde Zatera <zatera1920@gmail.com>
2017-06-24 05:30:11 +0700

    update app structure

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav className="nav has-shadow">
+          <div className="nav-left">
+            <a className="nav-item">
+              <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-zondez.appspot.com/o/icon-96x96.png?alt=media&token=3eaeeea0-b9c3-4002-9b45-f31acef86515" alt="test" />
+              Drunk Limit
+            </a>
+          </div>
+          <div className="nav-center">
+            <a className="nav-item">
+              <span className="icon">
+                test1
+              </span>
+            </a>
+            <a className="nav-item">
+              <span className="icon">
+                test
+              </span>
+            </a>
+          </div>
+          <div className="nav-right nav-menu">
+          </div>
+        </nav>
       </div>
     )
   }
 }
 
 export default App
+
