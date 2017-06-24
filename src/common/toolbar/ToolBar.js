d956ed6d74f7b450250d0e7b4f4eb00778bee7f0
Zonde Zatera <zatera1920@gmail.com>
2017-06-24 17:26:13 +0700

    cannot build maybe change ui theme

--- src/common/toolbar/ToolBar.js
+++ src/common/toolbar/ToolBar.js
@@ -1,57 +1,53 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { drawer } from 'material-components-web'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { actions } from 'redux-router5'
-import Link from './Link'
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+// // import { drawer } from 'material-components-web'
+// import { connect } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+// import { actions } from 'redux-router5'
 
-class ToolBar extends Component {
-  constructor(props) {
-    super(props)
-    this.openDrawer = this.openDrawer.bind(this)
-  }
-  openDrawer() {
-    this.drawer.open = true
-  }
-  componentDidMount() {
-    this.drawer = new drawer.MDCTemporaryDrawer(this.refs.drawer)
-  }
-  render() {
-    const { navigateTo } = this.props
-    return (
-      <header id="header" className="mdc-toolbar">
-        <div className="mdc-toolbar__row">
-          <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-            <button className="material-icons mdc-toolbar__icon--menu" onClick={() => this.openDrawer()}>menu</button>
-          </section>
-        </div>
-        <aside className="mdc-temporary-drawer" ref="drawer">
-          <nav className="mdc-temporary-drawer__drawer">
-            <nav className="mdc-temporary-drawer__content mdc-list">
-              <div className="mdc-list-item" onClick={() => console.log('test')}>
-                <Link router={ this.router } navigateTo={ navigateTo } name='inbox' options={{ reload: true }}>Inbox</Link>
-              </div>
-              <div className="mdc-list-item" onClick={() => console.log('test')}>Dashboard</div>
-              <div className="mdc-list-item" onClick={() => console.log('test')}>History</div>
-              <div className="mdc-list-item" onClick={() => console.log('test')}>About</div>
-            </nav>
-          </nav>
-        </aside>
-      </header>
-    )
-  }
-}
+// class ToolBar extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.openDrawer = this.openDrawer.bind(this)
+//   }
+//   openDrawer() {
+//     this.drawer.open = true
+//   }
+//   componentDidMount() {
+//     this.drawer = new drawer.MDCTemporaryDrawer(this.refs.drawer)
+//   }
+//   render() {
+//     return (
+//       <header id="header" className="mdc-toolbar">
+//         <div className="mdc-toolbar__row">
+//           <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+//             <button className="material-icons mdc-toolbar__icon--menu" onClick={() => this.openDrawer()}>menu</button>
+//           </section>
+//         </div>
+//         <aside className="mdc-temporary-drawer" ref="drawer">
+//           <nav className="mdc-temporary-drawer__drawer">
+//             <nav className="mdc-temporary-drawer__content mdc-list">
+//               <div className="mdc-list-item" onClick={() => console.log('test')}>Home</div>
+//               <div className="mdc-list-item" onClick={() => console.log('test')}>Dashboard</div>
+//               <div className="mdc-list-item" onClick={() => console.log('test')}>History</div>
+//               <div className="mdc-list-item" onClick={() => console.log('test')}>About</div>
+//             </nav>
+//           </nav>
+//         </aside>
+//       </header>
+//     )
+//   }
+// }
 
-ToolBar.propTypes = {
-  navigateTo: PropTypes.func.isRequired
-}
+// ToolBar.propTypes = {
+//   navigateTo: PropTypes.func.isRequired
+// }
 
-ToolBar.contextTypes = {
-  router: PropTypes.object.isRequired
-}
+// ToolBar.contextTypes = {
+//   router: PropTypes.object.isRequired
+// }
 
-export default connect(
-  state => state.router.route,
-  dispatch => bindActionCreators({ navigateTo: actions.navigateTo }, dispatch)
-)(ToolBar)
+// export default connect(
+//   state => state.router.route,
+//   dispatch => bindActionCreators({ navigateTo: actions.navigateTo }, dispatch)
+// )(ToolBar)
