cc421af85e953ffed3f1db3c7264f44a3f73654a
Zonde Zatera <zatera1920@gmail.com>
2017-06-24 13:13:54 +0700

    update firebase config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
+import * as firebase from 'firebase'
+import registerServiceWorker from './utils/registerServiceWorker'
 import './theme/index.css'
 import App from './App'
-import registerServiceWorker from './utils/registerServiceWorker'
+
+const config = {
+  apiKey: 'AIzaSyCT4dOTEjbQ4Z5JwpZYMd4S0PJiLOvLuKY',
+  authDomain: 'pwa-hackathon-67271.firebaseapp.com',
+  databaseURL: 'https://pwa-hackathon-67271.firebaseio.com',
+  projectId: 'pwa-hackathon-67271',
+  storageBucket: '',
+  messagingSenderId: '419104359716'
+}
+
+firebase.initializeApp(config)
+window.$firebase = firebase
 
 ReactDOM.render((
   <BrowserRouter>
